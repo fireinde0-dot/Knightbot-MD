aa5da4b4728afaabee3d54d9e1cee82e5842ed69
fireinde0-dot <fireinde0@gmail.com>
2026-02-16 23:54:20 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'NAVADOR Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "NAVADOR POWER Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2250506880574', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
